46fe23fda01d4f8ed414b2d940e15e33ebd2dc6e
juanmaperez <juanmaperezvar@gmail.com>
2019-04-06 21:24:48 +0100

    starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,6 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,63 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import "./../styles/global.scss"
+import "./../styles/global.css"
+
+import logo from './../images/logo_pill.png';
+
+import styled from 'styled-components';
+
+const HeaderView = styled.header`
+  padding: 0px;
+  color: #000;
+  max-width: 100%;
+  
+  .header-wrapper {
+    display: flex;
+    flex-direction;
+    justify-content: flex-start;
+    padding: 15px 20px;
+    box-sizing: border-box;
+    max-width: 100%;
+
+    .logo {
+      margin-top: 3px;
+      width: 35px;
+      height: 24px;
+    }
+    
+    .title {
+      margin-left: 5px;
+      font-size: 26px;
+      line-height: 1.1;
+      font-weight: 800;
+      color: #000;
+      display: inline-block;
+      font-family: 'Questrial', sans-serif;
+
+      .title-link {
+        color: #000;
+        letter-spacing: 1.4px;
+        -webkit-text-stroke: 0.3px #000;
+        text-decoration: none;
+      }
+    }
+  }
+`
+
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <HeaderView>
+    <div className="header-wrapper">
+      <img className="logo" alt="encapsulated logotype" src={logo}/>    
+      <h1 className="title">
+        <Link className="title-link"
+          to="/">
           {siteTitle}
         </Link>
       </h1>
     </div>
-  </header>
+  </HeaderView>
 )
 
 Header.propTypes = {
