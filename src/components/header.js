e9276383535cb8583ae35789ee03a201fcd478fd
juanmaperez <juanmaperezvar@gmail.com>
2019-04-06 17:16:46 +0100

    starter point

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,11 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./../styles/global.scss"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
