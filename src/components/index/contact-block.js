4b927cebebaa9526bee261f139ec520eef7738e9
juanmaperez <juanmaperezvar@gmail.com>
2019-04-30 20:37:46 +0900

    add links to contact

--- src/components/index/contact-block.js
+++ src/components/index/contact-block.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const ContactBlockView = styled.div`
-  height: ${props => props.height}px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-
+  padding: 50px 0px;
   .title-box {
-    width: 50%;
+    width: 100%;
     padding: 20px;
     box-sizing: border-box;
+    margin-bottom: 30px;
+
     .contact-title {
       font-size: 8vw;
       margin: 50px 0px 0px;
@@ -26,18 +23,26 @@ const ContactBlockView = styled.div`
       color: transparent;
       -webkit-text-stroke: 0.001em var(--tertiaryColor);
     }
-    .contact-resume {
-      margin-top: 30px;
-      line-height: 1.4;
-      font-size: 20px;
+  }
+  .content {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    .hi-box {
+      width: 50%;
       padding: 20px;
-      letter-spacing: 1px;
-      p {
-        max-width: 90%;
-        margin-bottom: 20px;
-        strong {
-          color: var(--secondaryColor);
-          &.contact-btn { text-decoration: underline }
+      box-sizing: border-box;
+      .contact-resume {
+        line-height: 1.4;
+        font-size: 20px;
+        letter-spacing: 1px;
+        p {
+          max-width: 90%;
+          margin-bottom: 20px;
+          strong {
+            color: var(--secondaryColor);
+            &.contact-btn { text-decoration: underline }
+          }
         }
       }
     }
@@ -46,6 +51,16 @@ const ContactBlockView = styled.div`
     width: 50%;
     padding: 20px;
     box-sizing: border-box;
+    line-height: 1.4;
+    font-size: 20px;
+    letter-spacing: 1px;
+    ul {
+      padding: 30px 20px;
+      li {
+        margin-bottom: 10px;
+      }
+    }
+    
   }
 
   .message {
@@ -151,21 +166,30 @@ class ContactBlock extends Component {
         <div className="title-box">
           <h2 className="contact-title">Get in touch,</h2>
           <h2 className="contact-subtitle">don't be shy</h2>
-          <div className="contact-resume">
-            <p>You have a project or idea which must be developed in a <strong>online environment</strong>, tell me more about it.</p>
-            <p>Let me know what are you thinking about and we can decide together what's the <strong>best approach</strong> for making it live.</p>
-            <p>Get the best results and engage your target with an <strong>amazing website</strong> and the most accurate UX.</p>
-            <p>Contact by <strong className="contact-btn" onClick={this.getMyEmail}>email</strong> or <strong className="contact-btn" onClick={this.getMyPhone}>phone</strong></p>
-            <p></p>
+        </div>
+        <div className="content">
+          <div className="hi-box">
+            <div className="contact-resume">
+              <p>You have a project or idea which must be developed in a <strong>online environment</strong>, tell me more about it.</p>
+              <p>Let me know what are you thinking about and we can decide together what's the <strong>best approach</strong> for making it live.</p>
+              <p>Get the best results and engage your target with an <strong>amazing website</strong> and the most accurate UX.</p>
+              <p>Contact by <strong className="contact-btn" onClick={this.getMyEmail}>email</strong> or <strong className="contact-btn" onClick={this.getMyPhone}>phone</strong></p>
+              <p></p>
+            </div>
           </div>
-        </div>
-        <div className="social-box">
-          Get my social networks
-        </div>
+          <div className="social-box">
+            <p>If you rather something more social to talk with me, you can also find me on different social platforms throught the following links:</p>
+            <ul>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/juanmaperez">Github</a></li>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/juanmaperezvargas/">Linkedin</a></li>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://www.pinterest.co.uk/juanmaperezvar/">Pinterest</a></li>
+              <li><a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/7850072/juanmaperez">StackOverflow</a></li>
+            </ul>
+          </div>
+        </div>    
         { msg && <div className="message">
           { msg }
         </div>}
-
       </ContactBlockView>
     )
   }
