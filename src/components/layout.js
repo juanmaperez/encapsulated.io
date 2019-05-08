60b2e00485fac23f067d3e6d385b5c2f6dfb451a
juanmaperez <juanmaperezvar@gmail.com>
2019-05-08 07:47:49 +0100

    fix cagada path

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import "git aprismjs/themes/prism-solarizedlight.css";
+import "prismjs/themes/prism-solarizedlight.css";
 import "./../styles/global.css"
 
 import Header from "./header";
